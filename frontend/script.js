7c7334c24df9d48b405339f2231834480f85a2eb
Hayden Daoust <Hayden.Daoust@unifycx.com>
2026-06-30 17:30:31 -0400

    front end with working access to microphone and can record audio to turn into temp url

---
+++ frontend/script.js
@@ -0,0 +1,50 @@
+console.log("JS IS RUNNING")
+
+const recordButton = document.querySelector("button")
+let recording = false
+let audioChunks = []
+let audioInput; 
+let blob;
+
+
+//asking for microphone acess and printing that you need it 
+async function micAcess (params) {
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        audioInput = new MediaRecorder(stream); 
+        console.log("Microphone Acess Greanted")
+
+        audioInput.ondataavailable = (event) => {
+            audioChunks.push(event.data)
+        }
+
+        audioInput.onstop = (event) => {
+            blob = new Blob(audioChunks, {type: 'webm' }) 
+            const url = URL.createObjectURL(blob)
+            console.log(url)
+        }
+        
+    } catch (error){
+        console.log("Microphone Acess was not given")
+    }
+}
+micAcess()
+
+//chaning the button so that when recroding the button informs the user 
+function updateButton (){
+    if(!recording){
+        console.log("Audio Recording")
+        recordButton.innerText = "Stop Recording"
+        recording = true 
+        audioInput.start();
+
+    } else {
+        console.log("Audio Stopped Recording")
+        recordButton.innerText = "RECORD"
+        recording = false 
+
+        audioInput.stop()
+    }
+}
+
+recordButton.addEventListener("click", updateButton)
